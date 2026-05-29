0852dd28778b6907fa95d18b52b2df895b05fcab
EusebiuzPR <273243314+EusebiuzPR@users.noreply.github.com>
2026-05-28 22:58:04 -0500

    Trying my best

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    
+    let r = "";
+    for (let i = 0;i < num;i++) {
+        
+        r += string;
+    }
+    if (num < 0 ){
+        return "ERROR"
+    }
+        return r;
 
 };
-
 // Do not edit below this line
 module.exports = repeatString;
